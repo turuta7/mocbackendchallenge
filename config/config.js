e3b49ac977b568dee2f6e7109f857b10045ca08a
Turuta Aleksandr <turuta51@gmail.com>
2019-06-21 23:12:28 +0300

    add_Sequelize

--- config/config.js
+++ config/config.js
@@ -13,8 +13,13 @@ module.exports = {
     username: process.env.username,
     password: process.env.password,
     database: process.env.database,
-    host: '127.0.0.1',
+    host: process.env.host,
     dialect: 'postgres',
     operatorsAliases: false,
+    "dialectOptions": {
+      "ssl": true,
+      "native": true
+    }
+
   },
 };
